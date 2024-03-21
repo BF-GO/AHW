5f2b4f6b91dade1ab387ce67f8d37f742470ce62
BFGO <26bfgo@gmail.com>
2024-03-21 07:11:20 +0200

    Variables and rules for writing

---
+++ 76/main.js
@@ -0,0 +1,22 @@
+const firstName = 'John'
+const lastName = 'Smith'
+
+console.log(firstName, lastName)
+
+let userName = 'mcpadre'
+let age = 30
+let hobby = 'programming'
+let favoriteLanguage = 'JavaScript'
+let goal = 'to build web applications'
+
+console.log(userName, age, 'y.o', hobby, favoriteLanguage, goal)
+
+// Различие между const и let, const неизменяемая переменая
+
+// let namme = 'Jhon'
+// let admin
+
+// admin = namme
+
+// alert(namme)
+// alert(admin)
---
+++ 77/main.js
@@ -0,0 +1,21 @@
+const a = 5
+// + - * /
+const sum = 2 + 2
+console.log(sum)
+
+const minus = 10 - 5
+console.log(minus)
+
+const X = 10
+const Y = 20
+console.log(X * Y)
+
+const A = 10
+const B = 20
+const division = B / 2
+console.log(5 % 2)
+
+console.log('Hello ' + 'World')
+
+const num = 2
+console.log(-num)
