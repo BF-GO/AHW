21a12dcacc94f6adecb92c021c2c6b6a07616a17
BFGO <26bfgo@gmail.com>
2024-03-25 21:31:03 +0200

    Data types

---
+++ 78/main.js
@@ -0,0 +1,31 @@
+// 1) string
+// 2) number
+// 3) boolean
+// 4) undefined
+// 5) null
+// 6) bigint*
+// 7) symbol*
+
+const instrument = 'Инструменты'
+const box = 'ящик'
+console.log(`${instrument} лежит в ${box}e`)
+
+let tools = 'Ключ'
+const bags = 'сумке'
+tools = 10
+console.log(tools)
+
+const user = 'User'
+const age = 25
+const isAdmin = true
+console.log(`${user} admin: ${isAdmin}`)
+
+const coffee = 'Кофе'
+console.log(typeof coffee)
+
+let foo
+console.log(foo)
+
+let sum = null
+sum = 5 + 6
+console.log(sum)
