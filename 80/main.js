eda6949f7eb5fc3cd9c8298947db47ab06e7a620
BFGO <26bfgo@gmail.com>
2024-09-23 22:14:08 +0300

    Loops and iteration

---
+++ 80/main.js
@@ -0,0 +1,50 @@
+let i = 0;
+
+while (i < 10) {
+	i = i + 1;
+	console.log(i);
+}
+
+console.log('==============');
+
+let b = 0;
+do {
+	b++;
+	console.log(b);
+} while (b < 5);
+
+console.log('==============');
+
+for (let c = 1; c <= 10; c = c + 2) {
+	console.log(`Пройден ${c} круг`);
+}
+
+console.log('==============');
+
+for (let d = 2; d <= 12; d = d + 2) {
+	console.log(d);
+}
+
+console.log('==============');
+
+for (let g = 10; g >= 1; g = g - 1) {
+	console.log(g);
+}
+
+console.log('==============');
+
+let s = 0;
+
+while (s < 12) {
+	s = s + 2;
+	console.log(s);
+}
+
+console.log('==============');
+
+let m = 10;
+
+while (m >= 1) {
+	console.log(m);
+	m = m - 1;
+}
