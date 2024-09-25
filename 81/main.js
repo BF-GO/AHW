32de3519ab3f41b8cbd65b033affe588553069a1
BFGO <26bfgo@gmail.com>
2024-09-25 23:57:46 +0300

    Array

---
+++ 81/main.js
@@ -0,0 +1,13 @@
+const numbers = [1, 2, 3, 4, 5];
+
+numbers[4] = 6;
+
+console.log(numbers[2]);
+
+console.log('==============');
+
+const number = [1, 2, 3, 4, 5];
+
+for (let i = 0; i < number.length; i++) {
+	console.log(number[i] + 1);
+}
